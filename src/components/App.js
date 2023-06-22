663f3e4e34c0466981b35aae2535ac32a71ddc9f
Polash Biswas <polashbiswasit19@gmail.com>
2023-06-23 01:09:39 +0530

    submitting code-Fri Jun 23 2023 01:09:25 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,66 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
+let cities = [
+  "Mumbai",
+  "Delhi",
+  "Bangalore",
+  "Chennai",
+  "Kolkata",
+  "Hyderabad",
+  "Jaipur",
+  "Ahmedabad",
+  "Pune",
+  "Surat",
+  "Lucknow",
+  "Kanpur",
+  "Nagpur",
+  "Indore",
+  "Thane",
+  "Bhopal",
+  "Visakhapatnam",
+  "Patna",
+  "Vadodara",
+  "Ludhiana"
+]
+
 const App = () => {
+  let [inputData, setInputData] = useState("");
+  let [search, setsearch] = useState("");
+
+  useEffect(() => {
+    setsearch(cities.filter((city) => city.toLowerCase().includes(inputData.toLowerCase())));
+
+  }, [inputData])
+  // let suggestions = cities.filter((city)=> city.toLowerCase().includes(inputData.toLowerCase()));
+
+  function handelCity(value) {
+    setInputData(value);
+  }
+
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Search cities of India</h1>
+      <input
+        type="text"
+        onChange={(e) => setInputData(e.target.value)}
+        value={inputData}
+        className="searchBar"
+      />
+      {
+        inputData !== "" && search.length > 0 &&
+        (
+          <div className="city-container">
+            {
+              search.map((value, index) => (
+                <p key={index} onClick={() => handelCity(value)}>{value},{''}</p>
+              ))
+            }
+          </div>
+        )
+      }
     </div>
   )
 }
